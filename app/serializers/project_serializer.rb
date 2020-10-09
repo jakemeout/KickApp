3441978aa0e063608c16e3f908d55c100a210fcd
jakemeout <jacobosity@gmail.com>
2020-10-09 17:52:10 -0400

    Start with create-react-app

--- app/serializers/project_serializer.rb
+++ app/serializers/project_serializer.rb
@@ -1,5 +1,2 @@
-# class ProjectSerializer < ActiveModel::Serializer
-#   attributes :project_submitter_id, :project_developer_id, :project_name, :project_problem_statement, :project_idea_summary, :is_claimed, :project_start_date, :project_end_date, :project_started, :in_progress, :completed, :abandoned, :abandoned_date, :archived, :archived_date, :num_up_votes, :num_down_votes, :sponsor_amount, :tag_name
-
-
-# end
+class ProjectSerializer < ActiveModel::Serializer
+end
